f7891366601ea46563734e82194f571f61df07cb
android-abukhalid <android.abukhalid@gmail.com>
2015-11-26 01:46:19 +0600

    commit

---
+++ VoterRegistation2/gen/com/aapbd/voterregistration/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.aapbd.voterregistration;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ VoterRegistation2/src/com/aapbd/voterregistration/YourNameActivity.java
@@ -0,0 +1,66 @@
+package com.aapbd.voterregistration;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+
+import com.aapbd.voterregistration.utils.AlertMessage;
+
+public class YourNameActivity extends Activity {
+
+	private Context con;
+	private EditText lastName, firstname, middlename, suffix;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.yourname);
+		con = this;
+		lastName = (EditText) findViewById(R.id.LastName);
+		firstname = (EditText) findViewById(R.id.FirstName);
+		middlename = (EditText) findViewById(R.id.MiddleName);
+		suffix = (EditText) findViewById(R.id.Suffix);
+
+	}
+
+	public void setCancel(View v) {
+		finish();
+	}
+
+	public void setBack(View v) {
+		Intent next = new Intent(con, CheckallThatApplyActivity.class);
+		startActivity(next);
+		finish();
+
+	}
+
+	public void setNext(View v) {
+		checkData();
+
+	}
+
+	public void checkData() {
+		if (TextUtils.isEmpty(lastName.getText().toString().trim())) {
+			AlertMessage.showMessage(con, getString(R.string.Status),
+					getString(R.string.lastName));
+			return;
+		}
+		if (TextUtils.isEmpty(firstname.getText().toString().trim())) {
+			AlertMessage.showMessage(con, getString(R.string.Status),
+					getString(R.string.firstName));
+			return;
+		} else {
+			Intent next = new Intent(con, BirthdayAndSexActivity.class);
+			startActivity(next);
+			finish();
+		}
+
+	}
+
+}
